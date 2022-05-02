1c7fca5252059d33343b8dec450858c7ea55fa20
anzar00 <peerzada.anzar@gmail.com>
2022-05-02 18:31:53 +0530

    init

---
+++ footer.php
@@ -0,0 +1,64 @@
+ <!--  footer -->
+ <footer id="contact">
+         <div class="footer">
+            <div class="container">
+               <div class="row">
+                  <div class="col-md-6">
+                     <div class="titlepage">
+                        <h2>Contact Us</h2>
+                     </div>
+                     <div class="cont">
+                        <h3>Fill the form or mail us at <br><a style="color: white;" href="mailto:email@domain.com">email@domain.com</a> </h3>
+                        <h2 style="color: white;">Or make a call at 
+                           <ul style="color: white;">
+                              <li><a href="tel:1234567890" style="color: white;">- 1234567890</a></li>
+                              <li><a href="tel:1234567890" style="color: white;">- 1234567890</a></li>
+                           </ul>
+                        </h2>
+                     </div>
+                  </div>
+                  <div class="col-md-6">
+                     <form id="request" class="main_form">
+                        <div class="row">
+                           <div class="col-md-12 ">
+                              <input class="contactus" placeholder="Full Name" type="type" name="Full Name"> 
+                           </div>
+                           <div class="col-md-12">
+                              <input class="contactus" placeholder="Email" type="type" name="Email"> 
+                           </div>
+                           <div class="col-md-12">
+                              <input class="contactus" placeholder="Phone Number" type="type" name="Phone Number">                          
+                           </div>
+                           <div class="col-md-12">
+                              <textarea class="textarea" placeholder="Message" type="type" Message="Name">Message </textarea>
+                           </div>
+                           <div class="col-sm-12">
+                              <button class="send_btn">Send</button>
+                           </div>
+                        </div>
+                     </form>
+                  </div>
+               </div>
+            </div>
+            <div class="copyright">
+               <div class="container">
+                  <div class="row">
+                     <div class="col-md-12">
+                        <p>&copy; <span id="displayYear"></span>Barzam Resorts By - <a href="../index.php">Hightown Communities</a></p>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </footer>
+      <!-- end footer -->
+      <!-- Javascript files-->
+      <script src="js/jquery.min.js"></script>
+      <script src="js/popper.min.js"></script>
+      <script src="js/bootstrap.bundle.min.js"></script>
+      <script src="js/jquery-3.0.0.min.js"></script>
+      <script src="js/plugin.js"></script>
+      <!-- sidebar -->
+      <script src="js/jquery.mCustomScrollbar.concat.min.js"></script>
+      <script src="js/custom.js"></script>
+      <script src="https:cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.js"></script>
